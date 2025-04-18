c11f24ae4eef8ac688e331a1ae2d8e99e12cc0bc
Sindhu S <51540427+simanyas@users.noreply.github.com>
2025-04-18 17:30:03 +0000

    fix

--- mp_api/app/api.py
+++ mp_api/app/api.py
@@ -11,10 +11,10 @@ import numpy as np
 import pandas as pd
 from fastapi import APIRouter, HTTPException, Body
 from fastapi.encoders import jsonable_encoder
-
+from mp_model.config.core import TRAINED_MODEL_DIR, PRED_DIR
 from mp_model import __version__ as model_version
-from mp_model.predict import make_prediction
-
+#from mp_model.predict import make_prediction
+from tensorflow import keras
 from app import __version__, schemas
 from app.config import settings
 
@@ -41,6 +41,33 @@ example_input = {
         }
     ]
 }
+def make_prediction(test_image) -> dict:
+    """Make a prediction using a saved model """
+    reloaded_model = keras.models.load_model(TRAINED_MODEL_DIR / "mp__model_output_v0.0.1.keras")
+    label_names = ["partial_mask","with_mask","without_mask"]
+    img_tensor = get_img_array()
+    predicted = reloaded_model.predict(img_tensor)
+    predicted_id = np.argmax(predicted, axis=-1)
+    predicted_label = [label_names[idx] for idx in predicted_id]
+    errors = False
+    results = {"predictions": predicted_label, "version": _version, "errors": errors}
+    print("Results:", results, predicted)
+    return results
+
+# Function to preprocess the image into an array suitable for input into a model
+def get_img_array():
+    # Loading the image from the path and resizing it to the target size (180x180)
+    img = keras.utils.load_img(PRED_DIR / "IMG_0334.jpg", target_size=(256, 256))
+
+    # Converting the loaded image into a numpy array
+    array = keras.utils.img_to_array(img)  # Converts image to a 3D numpy array (height, width, channels)
+
+    # Adding an extra dimension to create a batch of one sample
+    # This changes the shape from (height, width, channels) to (1, height, width, channels)
+    array = np.expand_dims(array, axis=0)  # The shape is now (1, 180, 180, 3)
+
+    # Returning the processed image array
+    return array
 
 @api_router.post("/predict", response_model=schemas.PredictionResults, status_code=200)
 async def predict(input_data: schemas.MultipleDataInputs = Body(..., example=example_input)) -> Any:
@@ -49,7 +76,7 @@ async def predict(input_data: schemas.MultipleDataInputs = Body(..., example=exa
     """
     input_image_name = jsonable_encoder(input_data.inputs)
     print(input_image_name)
-    results = make_prediction(test_image=input_image_name)
+    results = make_prediction(input_image_name)
 
     if results["errors"] is not None:
         raise HTTPException(status_code=400, detail=json.loads(results["errors"]))
