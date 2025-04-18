1ae58b1f4df9fd90d2fd5cf52a74329da61112ca
Sindhu S <51540427+simanyas@users.noreply.github.com>
2025-04-18 11:55:11 +0530

    Add files via upload

---
+++ tests/test_predictions.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+import pytest
+from mp_model.train_pipeline import evaluate_model
+
+@pytest.fixture
+def sample_input_data():
+    evaluate_model()
+    return
