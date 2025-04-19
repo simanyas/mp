d28e8a0bac93ee9f48024de7552ad978aa3bf6fc
Sindhu S <51540427+simanyas@users.noreply.github.com>
2025-04-19 17:35:38 +0530

    Update features.py

--- mp_model/processing/features.py
+++ mp_model/processing/features.py
@@ -1,11 +1,7 @@
 from typing import List
 import sys
-import pandas as pd
 import numpy as np
 import tensorflow as tf
-
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import OneHotEncoder
 
 class CollectBatchStats(tf.keras.callbacks.Callback):
   def __init__(self):
